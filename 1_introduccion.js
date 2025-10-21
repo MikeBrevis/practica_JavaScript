e438727d628df0754508e49ad3ae07e23bc61150
MikeBrevis <michel.brevis@gmail.com>
2025-10-21 17:49:40 -0300

    finish introduction

---
+++ 1_introduccion.js
@@ -0,0 +1,39 @@
+/* Console log */
+
+console.log("Hola, js")
+
+//Llamar nombre de una variable
+const nombre = "Mike"
+console.log(nombre)
+
+/* Operador typeof */
+
+//Muestra varios valores y variables
+const apellido = "Brevis"
+console.log(nombre + " " + apellido) 
+
+// Devuelve en una cadena de texto el tipo de un operador
+const numero = 7
+typeof numero // "number"
+
+// Se peude usar directamente con los valores que se requieran comprobar
+typeof undefined
+typeof true
+typeof 42
+typeof "hola mundo"
+
+// Se puede usar con operadores de comparación
+const age = 42
+console.log(typeof age == "number") // true
+
+/* Null y undefined */
+
+// ambos valores solo pueden tener un valor
+
+let papel // Undefined = la variable no a sido definida
+let rollo = null // // Null = para que tenga el valor de Null debemos de asignarlo
+
+
+
+
+
--- 8_console_log.js
+++
@@ -1,9 +0,0 @@
-console.log("Hola, js")
-
-//Nombre de una variable
-const nombre = "Mike"
-console.log(nombre)
-
-//Muestra varios valores y variables
-const apellido = "Brevis"
-console.log(nombre + " " + apellido) 
