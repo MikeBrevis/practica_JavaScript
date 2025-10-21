e374f04f9f0eb9c25dfc86b881bef50c35a6c067
MikeBrevis <michel.brevis@gmail.com>
2025-10-20 23:01:43 -0300

    console log and typeof

---
+++ 9_operador_typeof.js
@@ -0,0 +1,17 @@
+// Devuelve en una cadena de texto el tipo de un operador
+
+const numero = 7
+typeof numero // "number"
+
+// Se peude usar directamente con los valores que se requieran comprobar
+
+typeof undefined
+typeof true
+typeof 42
+typeof "hola mundo"
+
+// Se puede usar con operadores de comparación
+
+const age = 42
+console.log(typeof age == "number") // true
+
