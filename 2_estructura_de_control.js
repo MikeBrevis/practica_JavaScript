6ac5d345cbf269891c02cef4fcbcd94af0a52354
MikeBrevis <michel.brevis@gmail.com>
2025-10-23 15:07:34 -0300

    control structure -- for, while --

--- 2_estructura_de_control.js
+++ 2_estructura_de_control.js
@@ -1,4 +1,4 @@
-/* codigo condicional con IF */
+/*  --------------------------------------------------------- codigo condicional con IF */
 
 const edad = 15
 
@@ -12,12 +12,12 @@ if (edad >= 18) {
     console.log("No es mayor de edad")
 }
 
-// Operadores logicon para la condicion
+// Operadores logicos 
 
 const edad2 = 16
 const carnet = true
 
-if (edad2 >= 18 && carnet) {
+if (edad2 >= 18 && carnet) { // se ejecuta si la edad es igual o mayor a 18 Y (&&) si carnet es true
     console.log("Puede conducir")
 } else {
     console.log("No puede conducir")
@@ -34,3 +34,114 @@ if (puedeConducir) {
 } else {
     console.log("No puede conducir")
 }
+
+/*  --------------------------------------------------------- bucles con WHILE */
+
+// Estrucura de control que permite repetir una accion tantas veces como se requera, siempre que se cumpla una condición
+
+let numeroWhile = 10
+
+while (numeroWhile >= 0) {
+    console.log("quedan " + numeroWhile)
+    numeroWhile = numeroWhile - 1
+}
+console.log("Despegue!")
+
+// Break para salir del bucle
+
+let numeroWhile2 = 10
+
+while (numeroWhile2 >= 0) {
+
+    console.log("quedan " + numeroWhile2)
+
+    if (numeroWhile2 == 5) { // se rompe el bucle al llegar a 5
+        break
+    }
+
+    numeroWhile2 = numeroWhile2 - 1
+}
+console.log("Se rompe cuando llega a 5")
+
+// Continue omite la ejecucion y salta a la siguiente iteración
+
+let numeroWhile3 = 10
+
+while (numeroWhile3 > 0) {
+    numeroWhile3--
+
+    if (numeroWhile3 % 2 == 0) { // omite todos los numero pares
+        continue
+    }
+    console.log("quedan " + numeroWhile3)
+}
+console.log("Se salta todos los numero pares!")
+
+/*  --------------------------------------------------------- bucles DO WHILE */
+// Se ejecuta al menos una vez antes de entrar al bucle
+
+//Podemos conbinarlo con una funcion integrada de js: confirm
+
+let respuesta
+
+/* do {
+    respuesta = confirm("quieres seguir en el codigo?") // produce true o false que podemos guardar en una variable
+} while (respuesta) // mientras sea true, se ejecuta el codigo
+ */
+
+/*  --------------------------------------------------------- Expresiones y declaraciones */
+
+let declarar = 10 // Sentencia que define una variable. No devuelve un valor
+console.log(declarar)
+
+console.log(2 + 2) // Se expresa un valor que devuelve un resultado
+
+/*  --------------------------------------------------------- Bucles FOR */
+
+// imprimir numeros del 1 al 10
+// se define: variable, condicion, actualizacion
+
+for (let number = 1; number <= 10; number++) {
+    console.log("vamos en el " + number)
+}
+
+for (let i = 10; i >= 0; i--) {
+    if (i == 0) {
+        console.log("Despegue!")
+    } else {
+        console.log("Faltan " + i + " segundos")
+    }
+}
+
+// continue y break
+
+for (let i = 0; i < 10; i++) {
+    const esPar = i % 2 === 0
+    if (esPar) {
+        continue
+    }
+
+    // se muestran solo los impares
+    console.log(i)
+
+    // break al llegar a 7
+    if (i === 7) {
+        break
+    }
+}
+
+
+// Bucles anidados con for 
+for (let i = 1; i <= 10; i++) { // pasa al siguiente valor de i una vez se cumplas las condiciones del for anidado
+    for (let j = 1; j <= 10; j++) {
+        const resultado = i * j
+        console.log(i + ' x ' + j + ' = ' + resultado)
+    }
+}
+
+
+
+
+
+
+
